14f0d0a13de797948c7cc420b4e4483e82cdc093
FatGeek <yuhrenhuang@gmail.com>
2018-07-19 09:58:29 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /**
  *
- * Gulpfile setup
+ * 步驟＃1：設置變量
  *
  * @since 1.0.0
  * @authors Ahmad Awais, @digisavvy, @desaiuditd, @jb510, @dmassiani and @Maxlopez
@@ -9,12 +9,12 @@
 
 
 // Project configuration
-var project 		= 'neat', // Project name, used for build zip.
-	url 		= 'neat.dev', // Local Development URL for BrowserSync. Change as-needed.
+var project 		= 'neat', // //項目名稱，用於構建zip。
+	url 		= 'neat.dev', // localhost的項目URL
 	bower 		= './assets/bower_components/'; // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
-	build 		= './buildtheme/', // Files that you want to package into a zip go here
+	build 		= './buildtheme/', // 要打包成zip的文件在這裡
 	buildInclude 	= [
-				// include common file types
+				// 包含常見文件類型
 				'**/*.php',
 				'**/*.html',
 				'**/*.css',
@@ -26,10 +26,10 @@ var project 		= 'neat', // Project name, used for build zip.
 				'**/*.woff',
 				'**/*.woff2',
 
-				// include specific files and folders
+				// 包含特定文件和文件夾
 				'screenshot.png',
 
-				// exclude files and folders
+				// 排除文件和文件夾
 				'!node_modules/**/*',
 				'!assets/bower_components/**/*',
 				'!style.css.map',
@@ -38,36 +38,35 @@ var project 		= 'neat', // Project name, used for build zip.
 
 			];
 
-// Load plugins
+// 加載Gulp插件
 	var gulp     = require('gulp'),
-		browserSync  = require('browser-sync'), // Asynchronous browser loading on .scss file changes
+		browserSync  = require('browser-sync'), // 驚人的節省時間的同步瀏覽器測試
 		reload       = browserSync.reload,
-		autoprefixer = require('gulp-autoprefixer'), // Autoprefixing magic
-		minifycss    = require('gulp-uglifycss'),
-		filter       = require('gulp-filter'),
-		uglify       = require('gulp-uglify'),
-		imagemin     = require('gulp-imagemin'),
-		newer        = require('gulp-newer'),
-		rename       = require('gulp-rename'),
-		concat       = require('gulp-concat'),
-		notify       = require('gulp-notify'),
-		cmq          = require('gulp-combine-media-queries'),
-		runSequence  = require('gulp-run-sequence'),
-		sass         = require('gulp-sass'),
-		plugins      = require('gulp-load-plugins')({ camelize: true }),
-		ignore       = require('gulp-ignore'), // Helps with ignoring files and directories in our run tasks
-		rimraf       = require('gulp-rimraf'), // Helps with removing files and directories in our run tasks
-		zip          = require('gulp-zip'), // Using to zip up our packaged theme into a tasty zip file that can be installed in WordPress!
-		plumber      = require('gulp-plumber'), // Helps prevent stream crashing on errors
-		cache        = require('gulp-cache'),
-		sourcemaps   = require('gulp-sourcemaps');
+		autoprefixer = require('gulp-autoprefixer'), // 舊版瀏覽器的自動前綴CSS
+		minifycss    = require('gulp-uglifycss'), //用於CSS縮小
+		filter       = require('gulp-filter'), //通過使用globbing過濾它們，使您能夠處理原始文件的子集。
+		uglify       = require('gulp-uglify'),//縮小JS文件
+		imagemin     = require('gulp-imagemin'),//縮小PNG，JPEG，GIF和SVG圖像
+		newer        = require('gulp-newer'), //僅用於傳遞比相應目標文件更新的源文件。
+		rename       = require('gulp-rename'), //輕鬆重命名文件
+		concat       = require('gulp-concat'), //連接JS文件
+		notify       = require('gulp-notify'), //基於節點通知模塊向OS發送通知
+		cmq          = require('gulp-combine-media-queries'), //在Sass或更少之後組合重複媒體查詢
+		runSequence  = require('gulp-run-sequence'), //按順序運行一系列從屬gulp任務
+		sass         = require('gulp-sass'), //基於libSass的Sass的 Gulp插件
+		plugins      = require('gulp-load-plugins')({ camelize: true }), //自動加載gulp插件
+		ignore       = require('gulp-ignore'), // 根據文件特徵忽略流中的文件
+		rimraf       = require('gulp-rimraf'), // 幫助我們在運行任務中刪除文件和目錄
+		zip          = require('gulp-zip'), // 使用將我們打包的主題壓縮成一個可以安裝在WordPress中的美味zip文件！
+		plumber      = require('gulp-plumber'), // 修復節點管道，防止它們因錯誤而損壞
+		cache        = require('gulp-cache'), //Gulp的緩存代理任務
+		sourcemaps   = require('gulp-sourcemaps'); //CSS部分文件的源地圖支持
 
 
 /**
- * Browser Sync
+ * Browser Sync瀏覽器同步
  *
- * Asynchronous browser syncing of assets across multiple devices!! Watches for changes to js, image and php files
- * Although, I think this is redundant, since we have a watch task that does this already.
+ * 異步瀏覽器跨多個設備同步資產!! 觀看js，圖像和php文件的更改 
 */
 gulp.task('browser-sync', function() {
 	var files = [
@@ -96,11 +95,11 @@ gulp.task('browser-sync', function() {
 
 
 /**
- * Styles
+ * 第四步：Gulp Style Task
  *
- * Looking at src/sass and compiling the files into Expanded format, Autoprefixing and sending the files to the build folder
+ * 查看src / sass並將文件編譯為Expanded格式，Autoprefixing並將文件發送到build文件夾
  *
- * Sass output styles: https://web-design-weekly.com/2014/06/15/different-sass-output-styles/
+ * Sass輸出樣式：https：//web-design-weekly.com/2014/06/15/different-sass-output-styles/
 */
 gulp.task('styles', function () {
 	 	gulp.src('./assets/css/*.scss')
@@ -121,9 +120,9 @@ gulp.task('styles', function () {
 				.pipe(sourcemaps.write('.'))
 				.pipe(plumber.stop())
 				.pipe(gulp.dest('./'))
-				.pipe(filter('**/*.css')) // Filtering stream to only css files
+				.pipe(filter('**/*.css')) // 將流過濾到只有css文件
 				.pipe(cmq()) // Combines Media Queries
-				.pipe(reload({stream:true})) // Inject Styles when style file is created
+				.pipe(reload({stream:true})) // 在創建樣式文件時注入樣式
 				.pipe(rename({ suffix: '.min' }))
 				.pipe(minifycss({
 					maxLineLen: 80
@@ -135,9 +134,9 @@ gulp.task('styles', function () {
 
 
 /**
- * Scripts: Vendors
+ * 步驟＃5：腳本縮小和連接 Scripts: Vendors
  *
- * Look at src/js and concatenate those files, send them to assets/js where we then minimize the concatenated file.
+ * 查看src / js並連接這些文件，將它們發送到assets / js，然後我們最小化連接文件。
 */
 gulp.task('vendorsJs', function() {
 	return 	gulp.src(['./assets/js/vendor/*.js', bower+'**/*.js'])
@@ -154,9 +153,9 @@ gulp.task('vendorsJs', function() {
 
 
 /**
- * Scripts: Custom
+ * 腳本：自定義 Scripts: Custom
  *
- * Look at src/js and concatenate those files, send them to assets/js where we then minimize the concatenated file.
+ * 查看src / js並連接這些文件，將它們發送到assets / js，然後我們最小化連接文件。
 */
 
 gulp.task('scriptsJs', function() {
@@ -174,13 +173,13 @@ gulp.task('scriptsJs', function() {
 
 
 /**
- * Images
+ * 步驟＃6：圖像優化任務 Images
  *
- * Look at src/images, optimize the images and send them to the appropriate place
+ * 查看src / images，優化圖像並將它們發送到適當的位置
 */
 gulp.task('images', function() {
 
-// Add the newer pipe to pass through newer images only
+// 添加較新的pipe以僅傳遞較新的圖像
 	return 	gulp.src(['./assets/img/raw/**/*.{png,jpg,gif}'])
 				.pipe(newer('./assets/img/'))
 				.pipe(rimraf({ force: true }))
@@ -191,7 +190,7 @@ gulp.task('images', function() {
 
 
 /**
- * Clean gulp cache
+ * 步驟＃7：構建一個乾淨的可安裝主題Zip文件 清理gulp緩存
  */
  gulp.task('clear', function () {
    cache.clearAll();
@@ -201,8 +200,8 @@ gulp.task('images', function() {
  /**
   * Clean tasks for zip
   *
-  * Being a little overzealous, but we're cleaning out the build folder, codekit-cache directory and annoying DS_Store files and Also
-  * clearing out unoptimized image files in zip as those will have been moved and optimized
+  * 有點過分熱心，但我們正在清理build文件夾，codekit-cache目錄和煩人的DS_Store文件。
+  * 清除zip中未優化的圖像文件，因為這些文件已被移動和優化
  */
 
  gulp.task('cleanup', function() {
@@ -219,10 +218,10 @@ gulp.task('images', function() {
  });
 
  /**
-  * Build task that moves essential theme files for production-ready sites
+  * 構建任務，為生產就緒站點移動基本主題文件
   *
-  * buildFiles copies all the files in buildInclude to build folder - check variable values at the top
-  * buildImages copies all the images from img folder in assets while ignoring images inside raw folder if any
+  * buildFiles將buildInclude中的所有文件複製到build文件夾 - 檢查頂部的變量值
+  * buildImages複製資產中img文件夾中的所有圖像，同時忽略原始文件夾中的圖像（如果有）
   */
 
   gulp.task('buildFiles', function() {
@@ -244,9 +243,9 @@ gulp.task('buildImages', function() {
 });
 
  /**
-  * Zipping build directory for distribution
+  * 壓縮構建目錄以進行分發
   *
-  * Taking the build folder, which has been cleaned, containing optimized files and zipping it up to send out as an installable theme
+  * 獲取已清理的build文件夾，其中包含優化文件並將其壓縮以作為可安裝主題發送
  */
  gulp.task('buildZip', function () {
  	// return 	gulp.src([build+'/**/', './.jshintrc','./.bowerrc','./.gitignore' ])
@@ -259,19 +258,19 @@ gulp.task('buildImages', function() {
 
  // ==== TASKS ==== //
  /**
-  * Gulp Default Task
+  * Gulp默認任務
   *
-  * Compiles styles, fires-up browser sync, watches js and php files. Note browser sync task watches php files
+  * 編譯樣式，啟動瀏覽器同步，監視js和php文件。注意瀏覽器同步任務監視php文件
   *
  */
 
- // Package Distributable Theme
+ // 包可分發主題
  gulp.task('build', function(cb) {
  	runSequence('styles', 'cleanup', 'vendorsJs', 'scriptsJs',  'buildFiles', 'buildImages', 'buildZip','cleanupFinal', cb);
  });
 
 
- // Watch Task
+ // 觀察任務
  gulp.task('default', ['styles', 'vendorsJs', 'scriptsJs', 'images', 'browser-sync'], function () {
  	gulp.watch('./assets/img/raw/**/*', ['images']);
  	gulp.watch('./assets/css/**/*.scss', ['styles']);
